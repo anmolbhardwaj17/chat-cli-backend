603776a8d50a000e3ae43d647cc20e963f591e65
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-01-16 20:11:48 +0530

    initial

--- chat-node.js
+++ chat-node.js
@@ -2,7 +2,10 @@ const http = require('http').createServer();
 const io = require('socket.io')(http);
 
 
-http.listen(process.env.PORT || 3000, () => console.log(`server listening on port: ${port}`))
+http.createServer(function (req, res) {
+    res.write('Hello World!'); //write a response
+    res.end(); //end the response
+  }).listen(process.env.PORT || 3000, () => console.log(`server listening on port: ${port}`))
 
 
 
